a69e78464f2e72fe0b65c4476ece49d2f5df3330
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2024-08-08 20:50:06 +0100

    setup cli class

---
+++ cli/cli_app.py
@@ -0,0 +1,9 @@
+class CliApp:
+    """
+    setting up the core stuff that we will be implementing via our cli application
+    """
+    def __init__(self) -> None:
+        pass
+
+    def parse_command(self, command:str) -> str:
+        pass
